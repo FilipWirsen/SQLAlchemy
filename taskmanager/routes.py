c6fa447d9e81f0b8aa9b38c7643cffefa58e13ca
FilipWirsen <Filip.wirsen99@gmail.com>
2022-04-13 17:19:11 +0000

    Rename taskmaneger to taskmanager

--- taskmanager/routes.py
+++ taskmanager/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template
-from taskmaneger import app, db
-from taskmaneger.models import Category, Task
+from taskmanager import app, db
+from taskmanager.models import Category, Task
 
 
 @app.route("/")
