ed9c5899b0d4568a5a5617fd9cabb85c8af2b51b
FilipWirsen <Filip.wirsen99@gmail.com>
2022-04-14 10:38:46 +0000

    Add add_task function and html

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -1,4 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
     var sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
-  });
+
+  // datepicker initalization
+let datepicker = document.querySelectorAll('.datepicker');
+M.Datepicker.init(datepicker, {
+  format: "dd mmmm, yyyy",
+  i18n: {done: "Select"}
+});
+
+// select initalization
+let selects = document.querySelectorAll('select');
+M.FormSelect.init(selects);
+
+});
